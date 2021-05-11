02141a258feabdf5a4e374d14591a1a03f305b41
Adrian Brodzik <adrianbrodzyk@gmail.com>
2021-05-11 15:49:14 +0200

    Add working HTTP server.

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,13 +1,54 @@
 #include "server.h"
 
 Server::Server() {
-    std::cout << "server constructor" << std::endl;
+    http.config.port = 1337;
+
+    http.default_resource["GET"] = [](auto response, auto) {
+        *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n";
+    };
+
+    http.default_resource["POST"] = [](auto response, auto) {
+        *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n";
+    };
+
+    http.resource["^/info$"]["GET"] = [](auto response, auto request) {
+        std::stringstream stream;
+        stream << "<h1>Request from " << request->remote_endpoint_address() << ":" << request->remote_endpoint_port() << "</h1>";
+
+        stream << request->method << " " << request->path << " HTTP/" << request->http_version;
+
+        stream << "<h2>Query Fields</h2>";
+        auto query_fields = request->parse_query_string();
+        for (auto &field : query_fields) {
+            stream << field.first << ": " << field.second << "<br>";
+        }
+
+        stream << "<h2>Header Fields</h2>";
+        for (auto &field : request->header) {
+            stream << field.first << ": " << field.second << "<br>";
+        }
+
+        response->write(stream);
+    };
 }
 
 Server::~Server() {
-    std::cout << "server destructor" << std::endl;
 }
 
 void Server::Run() {
-    std::cout << "server run" << std::endl;
+    http_thread = std::thread([&]() {
+        http.start();
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    SimpleWeb::Client<SimpleWeb::HTTP> client("localhost:1337");
+
+    try {
+        auto res = client.request("GET", "/info");
+        std::cout << res->content.string() << std::endl;
+    } catch (std::exception &) {
+    }
+
+    http_thread.join();
 }
--- src/server/server.h
+++ src/server/server.h
@@ -2,7 +2,14 @@
 
 #include <iostream>
 
+#include "simple-web-server/client_http.hpp"
+#include "simple-web-server/server_http.hpp"
+
 class Server {
+private:
+    SimpleWeb::Server<SimpleWeb::HTTP> http;
+    std::thread http_thread;
+
 public:
     Server();
     ~Server();
