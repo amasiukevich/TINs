4412b20278478ca614c8603b1b9377e671008a93
jsokolowska <joana.sokolowska@gmail.com>
2021-05-20 13:51:33 +0200

    Refactor

--- src/proxy/client_manager.h
+++ src/proxy/client_manager.h
@@ -23,22 +23,8 @@ public:
     ClientManager() = default;
     ~ClientManager() = default;
 
-    void QueueUpClient(int socket_fd){
-        boost::unique_lock<boost::mutex> lock(mutex);
-        clients.push(socket_fd);
-        cond.notify_one();
-    }   // mutex released automatically
-
-    // hangs if there is no clients waiting
-    int GetAwaitingClient(){
-        boost::unique_lock<boost::mutex> lock(mutex);
-        while(clients.empty()){
-            cond.wait(lock);
-        }
-        int result = clients.front();
-        clients.pop();
-        return result;
-    } //lock released here
+    void QueueUpClient(int socket_fd);
+    int GetAwaitingClient();
 
 };
 
