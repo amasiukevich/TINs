b3be43309b9ef3ac119ab96f69128acd28cf1c51
amasiukevich <anton.masiukiewicz1@gmail.com>
2021-06-07 19:17:58 +0200

    Mock device interface

--- src/device-mock/DeviceMock.cpp
+++ src/device-mock/DeviceMock.cpp
@@ -1,6 +1,9 @@
-#include "ProxyMock.h"
+//
+// Created by anton on 07.06.2021.
+//
 
-ProxyMock::ProxyMock(char const * fileName) {
+#include "DeviceMock.h"
+DeviceMock::DeviceMock(char const * filename) {
     std::ifstream file(filename);
     std::string line;
 
@@ -11,7 +14,9 @@ ProxyMock::ProxyMock(char const * fileName) {
     }
 
     config = load_config("config/global.json");
-    logger = init_logger("proxy-mock");
+    // TODO: get the id of the device here
+    logger = init_logger("device-mock");
+
     logger->flush_on(spdlog::level::info);
 
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
@@ -19,22 +24,27 @@ ProxyMock::ProxyMock(char const * fileName) {
         exit(-1);
     }
 
-    memset(&proxy_addr_for_devices, 0, sizeof(proxy_addr_for_devices));
-    proxy_addr_for_devices.sin_family = AF_INET;
-    proxy_addr_for_devices.sin_port = htons(config["proxy"]["port_for_devices"].GetInt());
-    proxy_addr_for_devices.sin_addr.s_addr = inet_addr(config["proxy"]["ip_for_devices"].GetString());
-    if (bind(sockfd, (const sockaddr *)&proxy_addr_for_devices, sizeof(proxy_addr_for_devices)) == -1) {
-        logger->error("Could not bind proxy address for devices");
+    // device address
+    memset(&device_addr, 0, sizeof(device_addr));
+    device_addr.sin_family = AF_INET;
+    // TODO: get the id of the device
+    device_addr.sin_port = htons(config["devices"]["device0"]["port"].GetInt());
+    device_addr.sin_addr.s_addr = inet_addr(config["devices"]["device0"]["ip"].GetString());
+
+    if (bind(sockfd, (const sockaddr *) &proxy_addr, sizeof(proxy_addr)) == -1) {
+        logger->error("Cannot bind proxy address");
         exit(-1);
     }
 
-    memset(&device_addr, 0, sizeof(device_addr));
-    device_addr.sin_family = AF_INET;
-    device_addr.sin_port = htons(config["devices"]["device0"]["port"].GetInt())
-    device_addr.sin_addr.s_addr = inet_addr(config["devices"]["device0"]["ip"].GetString());
+    // proxy address
+    memset(&proxy_addr, 0, sizeof(proxy_addr));
+    proxy_addr.sin_family = AF_INET;
+    proxy_addr.sin_port = htons(config["proxy"]["port_for_devices"].GetInt());
+    proxy_addr.sin_port = htons(config["proxy"]["ip_for_devices"].GetString());
 }
 
-ssize_t ProxyMock::SendPacket(AAA::PacketType type, char count, char session, std::string data) {
+
+ssize_t DeviceMock::SendPacket(AAA::PacketType type, char count, char session_id, std::string data) {
     char header[2] {0};
 
     AAA::SetType(header[0], type);
@@ -44,24 +54,30 @@ ssize_t ProxyMock::SendPacket(AAA::PacketType type, char count, char session, st
     std::string temp = header[1] + data;
     temp = header[0] + temp;
 
-    return sendto(sockfd, temp.c_str(), temp.size(), 0, (const sockaddr *) &device_addr, sizeof(device_addr));
+    return sendto(sockfd, temp.c_str(), temp_size, 0, (const sockaddr *) & device_addr, sizeof(device_addr));
 }
 
-ssize_t ProxyMock::ReceivePacket() {
+ssize_t DeviceMock::ReceivePacket() {
     memset(buffer, 0, sizeof(buffer));
     auto received = recv(sockfd, buffer, sizeof(buffer), 0);
-    logger->info("Received packet. Type {}, counter {}, session_id {}", getTypeAsStr(buffer[0]), (int) AAA::GetCount(buffer), (int) AAA::GetSessionId(buffer));
+    logger->info("Received packet. Type {}, counter {}, session_id {}",
+         getTypeAsStr(buffer[0]),
+         (int) AAA::GetCount(buffer),
+         (int) AAA::GetSessionId(buffer)
+    )
     return received;
 }
 
 
-void ProxyMock::Run() {
 
-    for (auto & cmd : commands) {
-        if (cmd.size() == 2) {
-            int microseconds = std::stoi(cmd[1]);
+void DeviceMock::Run() {
+    for (auto & command : commands) {
+
+        if (command.size() == 2) {
+            int microseconds = std::stoi(command[1]);
             usleep(microseconds);
-        } else if (cmd.size() == 3) {
+        } else if (command.size() == 3) {
+
             AAA::PacketType type;
             if (cmd[0] == "ERROR") {
                 type = AAA::PacketType::ERROR;
@@ -73,24 +89,26 @@ void ProxyMock::Run() {
                 close(sockfd);
                 throw std::runtime_error("Wrong packet type in instructions");
             }
+
             char session_id = (char) std::stoi(cmd[1]);
             char counter = (char) std::stoi(cmd[2]);
 
-            logger->info("Sending packet {} with count {} and session id {}", cmd[0], (int) counter, (int) session_id);
+            logger->info("Sending packet {} with count and session id {}", cmd[0], (int) counter, (int) session_id);
             SendPacket(type, counter, session_id, "test");
             ReceivePacket();
         } else {
             close(sockfd);
             logger->flush();
-            throw std::runtime_error("Unrecognized instructions");
+            throw std::runtime_error("Unknown instructions");
         }
     }
     close(sockfd);
     logger->flush();
 }
 
-std::string ProxyMock::getTypeAsStr(char buffer) {
+std::string DeviceMock::getTypeAsStr(char buffer) {
     auto type = AAA::GetType(buffer);
+
     if (type == AAA::PacketType::ACK) {
         return "ACK";
     }
@@ -102,5 +120,6 @@ std::string ProxyMock::getTypeAsStr(char buffer) {
     if (type == AAA::PacketType::ERROR) {
         return "ERROR";
     }
+
     return "UNKNOWN";
 }
---
+++ src/device-mock/DeviceMock.h
@@ -0,0 +1,49 @@
+//
+// Created by anton on 07.06.2021.
+//
+
+#ifndef AAA_PROTOCOL_DEVICEMOCK_H
+#define AAA_PROTOCOL_DEVICEMOCK_H
+
+#include <common/aaa.h>
+#include <netinet/in.h>
+#include <rapidjson/document.h>
+#include <spdlog/logger.h>
+#include <unistd.h>
+
+#include <vector>
+#include "common/utility.h"
+#include "device/device.h"
+
+class DeviceMock {
+
+public:
+    explicit DeviceMock(char const *);
+    void Run();
+
+private:
+
+    rapidjson::Document config;
+    std::shared_ptr<spdlog::logger> logger;
+
+    std::vector<std::vector<std::string>> commands;
+
+    std::string id;
+    uint max_packet_size;
+    char session_id;
+    char packet_counter = (char) -1;
+
+    int sockfd{};
+    sockaddr_in device_addr;
+    sockaddr_in proxy_addr;
+
+    char buffer[AAA_MAX_PACKET_SIZE];
+
+    ssize_t SendPacket(AAA::PacketType type, char count, std::string data);
+    ssize_t ReceivePacket();
+
+    std::string getTypeAsStr(char);
+};
+
+
+#endif  //AAA_PROTOCOL_DEVICEMOCK_H
